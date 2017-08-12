6e7e1270c304caa50adeaa6a4916f9ab64089616
Rossonero <zhihangyu@gmail.com>
2017-08-12 15:47:48 +0800

    add timer

--- testFrame.js
+++ testFrame.js
@@ -1,7 +1,9 @@
 
 function run(solution, testcases){
-    testcases.forEach(function(testcase, idx){
+    testcases.forEach(function(testcase, idx, timer){
+        if(timer) console.time('case ' + idx);
         let answer = solution(testcase);
+        if(timer) console.timeEnd('case '+idx);
         console.log('the answer for case %d is ', idx,answer);
     });
 }
