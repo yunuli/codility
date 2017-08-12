95215316d43ae22d2ce64fc43d1c531491033369
Rossonero <zhihangyu@gmail.com>
2017-08-12 19:01:28 +0800

    L10. counting coins

---
+++ L10.countingCoins.js
@@ -0,0 +1,9 @@
+/**
+ Consider n coins aligned in a row. Each coin is showing heads at the beginning.
+ Then, n people turn over corresponding coins as follows. Person i reverses coins with numbers that are multiples of i. That is, person i flips coins i, 2 · i, 3 · i, . . . until no more appropriate coins remain.
+count the number of coins showing tail
+ Time complexity :
+ O(log(n)) or
+ O(1): use math operation on floating point numers
+
+ */
