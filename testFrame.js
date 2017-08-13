98ec5fbafb10b1ddafe1bebe905df3a672d13e5f
Rossonero <zhihangyu@gmail.com>
2017-08-13 11:45:38 +0800

    refined test frame

--- testFrame.js
+++ testFrame.js
@@ -2,7 +2,7 @@
 function run(solution, testcases){
     testcases.forEach(function(testcase, idx, timer){
         if(timer) console.time('case ' + idx);
-        let answer = solution(testcase);
+        let answer = solution.apply(this,testcase);
         if(timer) console.timeEnd('case '+idx);
         console.log('the answer for case %d is ', idx,answer);
     });
