61ff98169ed821e2f2851cb1d72e0901e683c4b6
Rossonero <zhihangyu@gmail.com>
2017-08-14 00:29:35 +0800

    L13.fibfrog

--- L13.FibFrog.js
+++ L13.FibFrog.js
@@ -52,3 +52,110 @@
  N is an integer within the range [0..100,000];
  each element of array A is an integer that can have one of the following values: 0, 1.
  */
+//todo remember 1:fib can be find in O(log(N)) time (using matrix production) or constant time (by formula);
+// todo 2: for(prop in obj) is much slower than for(; ;)
+let tester = require('./testFrame');
+
+function fibonacci(){
+
+    let cache = [0,1];
+    this.get = function fib(N){
+        if(cache[N]) return cache[N];
+        for(let i = cache.length; i <= N; i++){
+            cache[i] = (cache[i-1] + cache[i-2]);
+        }
+        return cache;
+    };
+    this.fibs = cache;
+}
+
+
+function solution (A){
+    // //console.log(A.slice(0,100), B.slice(0,100));
+    // console.log('case is :%j',A);
+    A.push(1);
+
+    let len = A.length;
+    let fn = new fibonacci();
+    fn.get(25);
+    let fibs = fn.fibs;
+
+    let stepRecords = new Array(len+1), steps = [], maxstep = fibs[fibs.length-1];
+    stepRecords[0] = 0;
+
+    for(let i = 2; i < fibs.length && fibs[i] <= len; i++){
+        steps.push(fibs[i]);
+    }
+    // console.log(steps);
+    
+    // console.time('inner');
+    for(let i = 1; i < len+1; i++){
+        // let possible = true;
+        // console.log(i-1,'-r------');
+        if(A[i-1]){
+            // possible = false;
+            let min = i;
+            for(let s = 0; s < steps.length; s++){
+                let comefrom = i - steps[s];
+                // console.log(fibstep,comefrom, stepRecords[comefrom]);
+                if(comefrom >= 0){
+                    let tmp = stepRecords[comefrom];
+                    if( tmp !== undefined && tmp < min)
+                        min = tmp;
+                }
+            }
+            if(min != i){
+                stepRecords[i] = min + 1;
+            }
+        }
+
+
+        // if(!possible) return -1;
+    }
+
+    // console.timeEnd('inner');
+        // console.log(stepRecords.slice(-100,-1));
+
+    return stepRecords[len] ? stepRecords[len]:-1;
+}
+
+
+let testcases = [
+    [
+        function(){
+        // let b = [0,1,0,1,0,1,0,1,0,1];
+        let b = [1,1,1,1,1,1,1,1,1,1];
+        // let b = [0,0,0,0,0,0,0,0,0,0,0];
+        // let b = [0,1,0,0,1,0,0,0,0,1];
+        for (var i = 0; i < 13; i++) {
+            b = b.concat(b);
+        }
+        return b;
+    }()]
+    ,
+    [[0,0,0,1,1,0,1,0,0,0,0]],//3
+    [[]],//1
+    [[1,1,1,1,1]],//2
+    [[0,0,0,0,1]],//2
+    [[0,0,0,1,0]],//-1
+    [[0,0,1,0,0]],//2
+    [[0,1,0,1,0]],//3
+    [[1,0,0,0,0]],//2
+    [[0,0,0]],//-1
+    [[0,0]],//1
+    [[0]],//1
+    [[1,1,1]],//2
+    [[1,1]],//1
+    [[1]]//1
+];
+
+// let a = new fibonacci();
+// let b = [];
+// for(let i = 0; i< 100; i++){
+//     let temp1 = a.get(i);
+//     let temp2 = b[i] = a.get(i) % 2048;
+//     //console.log(i,temp1, temp2);
+// }
+// //console.log(b);
+
+tester.run(solution, testcases);
