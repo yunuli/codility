a5fa03dad404ef3474b3d7c3bfbb6b72a0e9bafd
Rossonero <zhihangyu@gmail.com>
2017-08-14 00:28:18 +0800

    add total timer

--- testFrame.js
+++ testFrame.js
@@ -1,11 +1,13 @@
 
 function run(solution, testcases){
+    console.time('total');
     testcases.forEach(function(testcase, idx, timer){
         if(timer) console.time('case ' + idx);
         let answer = solution.apply(this,testcase);
         if(timer) console.timeEnd('case '+idx);
         console.log('the answer for case %d is ', idx,answer);
     });
+    console.timeEnd('total');
 }
 
 exports.run = run;
