2e0551f03bfe483abc1b2d7fa162263df54e2498
Rossonero <zhihangyu@gmail.com>
2017-08-13 14:43:41 +0800

    add sum of reciprocals(multiplicative inverses) time complexity

--- L10.CountingFlippingCoins.js
+++ L10.CountingFlippingCoins.js
@@ -2,6 +2,7 @@
  Consider n coins aligned in a row. Each coin is showing heads at the beginning.
  Then, n people turn over corresponding coins as follows. Person i reverses coins with numbers that are multiples of i. That is, person i flips coins i, 2 · i, 3 · i, . . . until no more appropriate coins remain.
 count the number of coins showing tail
+ The sums of multiplicative inverses (reciprocals) of the first n numbers are called harmonic numbers, which asymptotically equal O(logn).
  Time complexity :
  O(log(n)) or
  O(1): use math operation on floating point numers
