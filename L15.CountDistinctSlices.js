c6f36a7b22290ab1bfb6d4eed0365129f7e671bd
Rossonero <zhihangyu@gmail.com>
2017-08-14 19:43:22 +0800

    L15 CountDistinctSlice good practice

--- L15.CountDistinctSlices.js
+++ L15.CountDistinctSlices.js
@@ -37,43 +37,82 @@
  each element of array A is an integer within the range [0..M].
  */
 let tester = require('./testFrame');
-
+function factorial(n){
+    let product = 1;
+    for(let i = 1; i <= n; i++){
+        product *= i;
+    }
+    return product;
+}
 function solution(M,A){
+    console.log(A);
+
     let map = new Array(M+1), len = A.length;
     for (let i = 0; i < map.length; i++) {
         map[i] = 0;
     }
-    let right=1,count=0,duplicates=0;
+    let left = 0,right=-1, count=0, duplicates=false;
+    let preleft = left, preright = right;
+    while(left < len && right < len){
+        let tmp;
+        // console.log('left,right:',left,right);
 
-    for(let left = 0; left < len; left++){
-        let tmp = map[A[left]]++;
-        if(tmp > 1){
-            duplicates++;
-        }
-
-        while(duplicates === 0){
-            while(right < len){
-                tmp = map[A[right]]++;
-                if(tmp === 1){
-                    right++;
-                }else{
-                    duplicates++;
-                    break;
-                }
+        while(!duplicates && ++right < len){
+            tmp = ++map[A[right]];
+            if(tmp > 1){
+                duplicates=true;
             }
         }
 
-        tmp = map[A[left]]--;
-        if(tmp ===1){
-            duplicates--;
+        console.log('mp',map.slice(0,10));
+
+
+        count += (right - left) * (right - left + 1) / 2;
+        console.log('count1:',count);
+
+        if(left < preright){
+            count -= (preright-left) * (preright-left + 1) /2;
         }
+        console.log('count2:',count);
+        preleft = left, preright = right;
+
+        if(count > 1000000000) return 1000000000;
+        // console.log(count, left, right);
+
+        while(duplicates){
+            tmp = --map[A[left]];
+            if(tmp ===1){
+                duplicates=false;
+            }
+            left++;
+        }
+        console.log('mp after',map.slice(0,10));
+
+        // console.log('left:',left);
 
     }
+    return count;
+
 }
 let testcases = [
-    [5,[3,4,5,2]],
-    [[]],
-    [[]]
+    [10000,[ 1, 3, 4, 1, 2, 1, 3, 2, 1 ]],
+    [5,[3,4,5,5,2]],
+    [6,[3,4,5,5,2]],
+    [5,[1,1,2,2,3,3]],
+    // ,
+    [10,[1,1,1,1,1,1,1,1]],
+    [10,[1,2,3,4,5,6,7]]
+    // ,
+    // [10,function(){
+    //     let a = [];
+    //     for (let i = 0; i < 10; i++) {
+    //         a[i] = Math.random();
+    //     }
+    //     return a;
+    // }()]
+    // // ,
+    // [[]],
+    // [[]]
 ];
 
 tester.run(solution,testcases);
