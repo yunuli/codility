16e9424aa018b06db64b51164a459379a9d60b08
Rossonero <zhihangyu@gmail.com>
2017-08-21 09:42:51 +0800

    L14 minmaxdivision

--- L14.MinMaxDivision.js
+++ L14.MinMaxDivision.js
@@ -46,3 +46,48 @@
  M is an integer within the range [0..10,000];
  each element of array A is an integer within the range [0..M].
  */
+
+let tester = require('./testFrame');
+
+let tester = require('./testFrame');
+
+//todo . it takes me a long time to get it correct. need practice more. 
+function check(A, K, value) {
+    let count = 1, len = A.length, sum = 0;
+    for (let i = 0; i < len; i++) {
+        sum += A[i];
+        if (sum > value) {
+            sum = A[i];
+            count++;
+        }
+        if (count > K) return false;
+    }
+    return true;
+}
+
+function solution(K, M, A) {
+    let mean = A.reduce((pre, cur) => pre + cur) / K;
+    let max = A[0];
+    A.forEach(item => max = item > max ? item : max);
+    let begin = Math.max(max, Math.ceil(mean)), end = mean + M, result = -1;
+    while (begin <= end) {
+        let mid = ~~((begin + end) / 2);
+        console.log(mid, begin, end);
+        if (check(A, K, mid)) {
+            end = mid - 1;
+            result = mid;
+        } else {
+            begin = mid + 1;
+        }
+    }
+    return result;
+}
+
+let testcases = [[3, 5, [2, 1, 5, 1, 2, 2, 2]], [10, 10, [10, 10, 10, 10, 10, 10, 10]], [10, 10, [10]], [1, 10, [1]], [1, 10, [10]], [2, 10, [10, 5]], [1, 10, [10, 5]], [3, 10, [10, 5]] 
+    //r //  , //  [], //  [], //  [], //  [415633212 ,234332], //  [ //    1000000000 ,1000000000 
+    //  ]
+     ]; 
+tester.run(solution, testcases);
+// 100111010000000011100101000110101010001011001000
+//                                             1101
+//                 11100101000110101010001011000101
