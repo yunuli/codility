915bd6fcb4530baefe98319fd73bea9704b90fb3
marcoyu <marcoyu@tencentcom>
2021-10-30 21:16:07 +0800

    feature:l401 超时版本

--- testFrame.js
+++ testFrame.js
@@ -1,21 +1,25 @@
-
-function run(solution, testcases){
+function run(solution, testcases, ...rest) {
     console.time('total');
-    testcases.forEach(function(testcase, idx, timer){
-        console.log('case %d start:----------------', idx);
+    testcases.forEach(function (testcase, idx, timer) {
+        console.info('case %d start:----------------', idx);
 
-        if(timer) console.time('case ' + idx);
-        if(testcase.length === 0) return;
-        try{
+        if (timer) console.time('case ' + idx);
+        if (testcase.length === 0) return;
+        try {
 
-        let answer = solution.apply(this,testcase);
-        if(timer) console.timeEnd('case '+idx);
-        console.log('the answer for case %d is ', idx,answer);
-        }catch (e) {
-            console.log(e);
+            let answer = solution.apply(this, testcase.concat(rest));
+            if (timer) console.timeEnd('case ' + idx);
+            console.info('the answer for case %d is ', idx, answer);
+        } catch (e) {
+            console.info(e);
         }
     });
     console.timeEnd('total');
+}
+
+let oldLog = console.log;
+console.log = function (...args) {
+    if (console.enableLog) oldLog(...args)
 }
 
 exports.run = run;
