a3b71944199c47eec2158c683ef300bf74afb4cb
marcoyu <marcoyu@tencentcom>
2021-12-16 20:35:52 +0800

    feature: L75 sort colors

---
+++ L75 Sort Colors.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=75 lang=javascript
+ *
+ * [75] Sort Colors
+ *
+ * https://leetcode.com/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (52.25%)
+ * Likes:    7938
+ * Dislikes: 361
+ * Total Accepted:    863.3K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * Given an array nums with n objects colored red, white, or blue, sort them
+ * in-place so that objects of the same color are adjacent, with the colors in
+ * the order red, white, and blue.
+ * 
+ * We will use the integers 0, 1, and 2 to represent the color red, white, and
+ * blue, respectively.
+ * 
+ * You must solve this problem without using the library's sort function.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * Example 2:
+ * Input: nums = [2,0,1]
+ * Output: [0,1,2]
+ * Example 3:
+ * Input: nums = [0]
+ * Output: [0]
+ * Example 4:
+ * Input: nums = [1]
+ * Output: [1]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == nums.length
+ * 1 <= n <= 300
+ * nums[i] is 0, 1, or 2.
+ * 
+ * 
+ * 
+ * Follow up: Could you come up with a one-pass algorithm using only constant
+ * extra space?
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    let counter = new Array(3).fill(0)
+    for (const num of nums) {
+        counter[num]++
+    }
+    let i = 0, pivot = 0;
+    for (let color = 0; color < counter.length; color++) {
+        pivot += counter[color];
+        for (; i < pivot; i++) {
+            nums[i] = color;
+        }
+    }
+};
+// @lc code=end
+
